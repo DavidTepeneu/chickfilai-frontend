5c6255b7b344f264e0da0b1dafd181abe8230a6b
David Tepeneu <david.tepeneu@gmail.com>
2024-10-24 17:39:38 -0500

    removed devtool disable

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
-import { disableReactDevtools } from "@fvilers/disable-react-devtools";
-
-if (process.env.NODE_ENV === "production") disableReactDevtools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
